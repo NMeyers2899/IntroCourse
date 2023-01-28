acff223d06ce367ba2b503237d24c53002b31280
Nicholas <nmeyers2899@gmail.com>
2023-01-28 12:10:35 -0600

    Added a Way to Print out a Character's Stats

--- Game.cs
+++ Game.cs
@@ -12,42 +12,59 @@ namespace HelloWorld
         float playerAttackPower = 100;
 
         // Enemy Stats
-        string enemyName = "Goblin";
-        float enemyHealth = 200;
-        float enemyAttackPower = 5;
+        string enemyName;
+        float enemyHealth;
+        float enemyAttackPower;
+
+        void InitalizeEnemy(string name, float health, float attackPower)
+        {
+            enemyName = name;
+            enemyHealth = health;
+            enemyAttackPower = attackPower;
+        }
 
         /// <summary>
         /// Pits the player against the enemy in a fight to the death.
         /// </summary>
         void Battle()
         {
+            // Holds what actions the player chooses to take.
             string playerChoice;
 
             Console.WriteLine("The " + enemyName + " stands before you. What shall you do?");
 
+            // While both the enemy and the player are still alive...
             while(enemyHealth > 0 && playerHealth > 0)
             {
+                // ...check to see what the player wishes to do.
                 Console.WriteLine("1. Attack \n2. Run Away");
                 playerChoice = Console.ReadLine();
 
+                // If the player picks the first or attack option...
                 if (playerChoice == "1" || playerChoice.ToLower() == "attack")
                 {
+                    // ...deal damage to the enemy.
                     Console.WriteLine("You slash out at the " + enemyName + "!");
                     enemyHealth -= playerAttackPower;
                     ClearScreen();
 
+                    // If the enemy is dead, exit the fight.
                     if (enemyHealth <= 0)
                         return;
 
+                    // If the enemy is still alive, the enemy damages the player.
                     Console.WriteLine("The " + enemyName + " lunges at you!");
                     playerHealth -= enemyAttackPower;
                     ClearScreen();
                 }
+                // If the player picks the second or run away option...
                 else if (playerChoice == "2" || playerChoice.ToLower() == "run away")
                 {
+                    // ...set up the player for a game over.
                     playerHealth = 0;
                     return;
                 }
+                // If the player inputs anything else, let them know it was an invalid input.
                 else
                 {
                     Console.WriteLine("Invalid Input");
@@ -66,12 +83,20 @@ namespace HelloWorld
             Console.Clear();
         }
 
+        void PrintCharacterStats(string name, float health, float attackPower)
+        {
+            Console.WriteLine(name + "'s Stats");
+            Console.WriteLine("Health : " + health + "\nAttack : " + attackPower);
+        }
+
         /// <summary>
         /// Player must fight a goblin.
         /// </summary>
         void RoomOne()
         {
             Console.WriteLine("Uh oh! Gablins!");
+
+            InitalizeEnemy("Goblin", 200, 2);
 
             Battle();
 
